0a308619d996aef7df3acb2ce891b072f96a0ca1
AidanLee <aidanl03@gmail.com>
2013-12-04 10:48:22 +1100

    Improved color parser:  -Now takes most input, pulls it apart and does it's best to figure out your color  -Guesses the color based on adjvectives used (only a few right now) and a decent list of colors  -Throws an exception if it can't find a matching color!  -Project now requires NLTK installed!
    Improved main loop:
     -Improved the test data.
     -Catches errors color parser throws.
    
    TODO:
     -Possibly display suggested color in an OGL window?
     -Increase the number of color names and adjectives.
     -Test further.

--- python/colorparser.py
+++ python/colorparser.py
@@ -5,7 +5,9 @@
         # This code falls under the GNU license
 
 from color import *;
+import nltk;
 
+    #RGB values for a lot of different colors.
 baseColors = [
     ["red",     0.7, 0.0, 0.0],
     ["blue",    0.0, 0.0, 0.7],
@@ -16,6 +18,7 @@ baseColors = [
     ["pink",    0.8, 0.4, 0.4],
     ["cyan",    0.0, 0.9, 0.9],
     ["teal",    0.0, 0.6, 0.6],
+    ["ocean",   0.3, 0.8, 0.8],
     ["magenta", 0.8, 0.0, 0.5],
     ["silver",  0.5, 0.5, 0.6],
     ["gold",    0.7, 0.7, 0.0],
@@ -26,12 +29,56 @@ baseColors = [
     ["white",   1.0, 1.0, 1.0],
     ["gray",    0.5, 0.5, 0.5],
     ["grey",    0.5, 0.5, 0.5] ]
+
+    #saturation adjectives increase or decrease saturation
+    #to do this we multiply the each RGB value by the number associated with the word.
+saturationAdjectives = [
+    ["vibrant", 1.3],
+    ["sharp", 1.3],
+    ["dull", 0.7],
+    ["cool", 0.9] ]
+
+    #brightness adjectives increase or decrease brightness, they add or subtract equally to each color, increasing brightness but losing some saturation.
+brightnessAdjectives = [
+    ["bright", 0.3],
+    ["light", 0.2],
+    ["dim", -0.2],
+    ["dark", -0.3] ]
         
     # display color suggestions. Intakes text, compares it to the color list then returns a color it feels is appropriate.
 def parseText(text):
-    for bcolor in baseColors:
-        if text == bcolor[0]:
-            print 'match found...';
-            return color(bcolor[1],bcolor[2],bcolor[3]); #returns the first exact match.
-    print 'no matches returning black';
-    return color(0.0,0.0,0.0);
+    text = text.lower(); #lower case it to avoid issues with capitalisation
+    sentence = nltk.word_tokenize(text);  #split the input into a list of words for processing.
+    red = 0.0;
+    blue = 0.0;
+    green = 0.0;
+    colorfound = False;
+        #find if any of our words match a color
+    for word in sentence:
+        for bcolor in baseColors: 
+            if word == bcolor[0]: 
+                red = bcolor[1];
+                green = bcolor[2];
+                blue = bcolor[3];
+                colorfound = True;
+                
+        #if any words match a saturation word...
+    for word in sentence:
+        for adj in saturationAdjectives:
+            if word == adj[0]: 
+                red = max(min(red*adj[1], 1.0), 0.0) #clamp between 0.0 and 1.0
+                green = max(min(green*adj[1], 1.0), 0.0)
+                blue = max(min(blue*adj[1], 1.0), 0.0)
+                
+        #if any words match a brightness word    
+    for word in sentence:
+        for adj in brightnessAdjectives:
+            if word == adj[0]:
+                red = max(min(red+adj[1], 1.0), 0.0) #clamp between 0.0 and 1.0
+                green = max(min(green+adj[1], 1.0), 0.0)
+                blue = max(min(blue+adj[1], 1.0), 0.0)
+
+    if (not colorfound):
+        raise NameError('ColorNotFound');
+                
+    return color(red, blue, green);
--- python/main.py
+++ python/main.py
@@ -11,34 +11,40 @@ from colorparser import *;
 def testColors():
     # test cases, left is input and right is roughly the expected output.
     tests = [
-        ["red",         "Color(1.0, 0.0, 0.0)"],
-        ["RED",         "Color(1.0, 0.0, 0.0)"],
-        ["Red",         "Color(1.0, 0.0, 0.0)"],
-        ["rEd",         "Color(1.0, 0.0, 0.0)"],
-        ["asdfa red",   "Color(1.0, 0.0, 0.0)"],
+        ["red",         "Color(0.7, 0.0, 0.0)"],
+        ["RED",         "Color(0.7, 0.0, 0.0)"],
+        ["Red",         "Color(0.7, 0.0, 0.0)"],
+        ["rEd",         "Color(0.7, 0.0, 0.0)"],
+        ["asdfa red",   "Color(0.7, 0.0, 0.0)"],
         ["asdfadsfred", "Error"],
-        ["blue",        "Color(1.0, 0.0, 0.0)"],
-        ["green",       "Color(1.0, 0.0, 0.0)"],
-        ["cyan",        "Color(1.0, 0.0, 0.0)"],
-        ["teal",        "Color(1.0, 0.0, 0.0)"],
-        ["yellow",      "Color(1.0, 0.0, 0.0)"],
-        ["purple",      "Color(1.0, 0.0, 0.0)"],
-        ["pink",        "Color(1.0, 0.0, 0.0)"],
-        ["bright red",  "Color(1.0, 0.0, 0.0)"],
-        ["dark red",    "Color(1.0, 0.0, 0.0)"],
-        ["dim red",     "Color(1.0, 0.0, 0.0)"],
-        ["light red",   "Color(1.0, 0.0, 0.0)"],
-        ["black",       "Color(1.0, 0.0, 0.0)"],
-        ["white",       "Color(1.0, 0.0, 0.0)"],
-        ["magenta",     "Color(1.0, 0.0, 0.0)"],
-        ["brown",       "Color(1.0, 0.0, 0.0)"] ]
+        ["blue",        "Color(0.0, 0.0, 0.7)"],
+        ["green",       "Color(0.0, 0.7, 0.0)"],
+        ["cyan",        "Color(0.0, 0.9, 0.9)"],
+        ["teal",        "Color(0.0, 0.7, 0.7)"],
+        ["yellow",      "Color(1.0, 1.0, 0.0)"],
+        ["purple",      "Color(0.7, 0.0, 0.7)"],
+        ["pink",        "Color(1.0, 0.4, 0.4)"],
+        ["bright red",  "Color(1.0, 0.1, 0.1)"],
+        ["dark red",    "Color(0.5, 0.0, 0.0)"],
+        ["dim red",     "Color(0.6, 0.0, 0.0)"],
+        ["light red",   "Color(1.0, 0.2, 0.2)"],
+        ["black",       "Color(0.0, 0.0, 0.0)"],
+        ["white",       "Color(0.0, 0.0, 0.0)"],
+        ["magenta",     "Color(0.8, 0.0, 0.6)"],
+        ["brown",       "Color(0.4, 0.3, 0.2)"] ]
     for test in tests:
         print ''
         print 'Input: {0}'.format(test[0]);
         print 'Expected Output:'
         print '{0}'.format(test[1]);
         print 'Actual Output: '
-        parseText(test[0]).printColor();
+        try:
+            parseText(test[0]).printColor();
+        except NameError:
+            print 'Error: No matching color';
+
+
+        
     # main loop.
 if __name__ == '__main__':
     end = False;
@@ -54,7 +60,11 @@ if __name__ == '__main__':
         color_raw = raw_input('Enter name of color:')
         if color_raw == "quit":
             end = True;
-        if color_raw == "test":
+        elif color_raw == "test":
             testColors();
-        red = color(1.0,0.0,0.0);
-        red.printColor();
+        else:
+            try:
+                c = parseText(color_raw);
+                c.printColor();
+            except NameError:
+                print 'Sorry, none of that was recognised as a color.';
